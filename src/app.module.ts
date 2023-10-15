4d29ba2593c7007ada966c5939891081bace00df
Aleksey Popov <mewehi.oiwy@gmail.com>
2023-10-15 21:37:31 +0700

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, PrismaModule, PostModule],
   controllers: [],
   providers: [],
 })
